e94630ac703b33b82a406c2817075aeb642844fb
Matthew Fitzsimmons <fitz.matt.d@gmail.com>
2017-03-25 11:11:03 +0000

    add zsh

---
+++ emacs/.emacs.d/elpa/auto-virtualenv-20170125.1117/auto-virtualenv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-virtualenv" "20170125.1117" "Auto activate python virtualenvs" '((cl-lib "0.5") (pyvenv "1.9") (s "1.10.0")) :url "http://github.com/marcwebbie/auto-virtualenv" :keywords '("python" "virtualenv" "tools"))
---
+++ emacs/.emacs.d/elpa/flycheck-mypy-20160220.1432/flycheck-mypy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-mypy" "20160220.1432" "Support mypy in flycheck" '((flycheck "0.18")))
